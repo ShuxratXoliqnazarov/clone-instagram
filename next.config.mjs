9e2972bb13c84501a2575167ff3fa000592cd377
Shuhrat <kholiqnazarovshuhrat@gmail.com>
2025-08-02 00:17:49 +0300

    fixing

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ["en", "tj"], // List of supported locales
-    defaultLocale: "en", // Default locale
-    localeDetection: false, // Automatically detect user language
-  },
+  // i18n: {
+  //   locales: ["en", "tj"], // List of supported locales
+  //   defaultLocale: "en", // Default locale
+  //   localeDetection: false, // Automatically detect user language
+  // },
   images: {
     domains: ["37.27.29.18"],
   },
