85aeb8dd604c31739ab2cc33b5bd42f500e98f04
Shuhrat <kholiqnazarovshuhrat@gmail.com>
2025-08-01 23:57:57 +0300

    fixing

--- src/api/pages/chat/utils/axios-reguest.js
+++ src/api/pages/chat/utils/axios-reguest.js
@@ -1,8 +1,12 @@
-import{ jwtDecode }from 'jwt-decode'
+import { jwtDecode } from 'jwt-decode'
 
-const token = typeof window !== 'undefined' ? localStorage.getItem('access_token') : null
-export const userId = typeof window !== 'undefined' ?  jwtDecode(token) : null
+const token =
+	typeof window !== 'undefined' ? localStorage.getItem('access_token') : null
 
-export const api = 'http://37.27.29.18:8003/'
+export const userId =
+	typeof window !== 'undefined' && token ? jwtDecode(token) : null
 
-export { token, userId, api }
+export const api = 'https://instagram-api.softclub.tj/'
+
+// Экспортируем token отдельно, так как userId и api уже экспортированы выше
+export { token }
